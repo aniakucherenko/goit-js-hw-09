c0dd65983a94f047d0751f038cd059fc48fc09cc
aniakucherenko <ania.kucherenko@gmail.com>
2022-11-14 20:48:53 +0200

    update

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,8 +2,19 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 
-const flatpickr = require("flatpickr"); 
-const fP = flatpickr("#datetime-picker", {options});
+const buttonStart = document.querySelector('button[data-start]');
+const timePicker =  document.getElementById('datetime-picker');
+const daysNumb = document.querySelector('[data-days]');
+const hoursNumb = document.querySelector('[data-hours]');
+const minutesNumb = document.querySelector('[data-minutes]');
+const secondsNumb = document.querySelector('[data-seconds]');
+// const setDate = {};
+
+let timerId = null;
+let selectedDate = null;
+buttonStart.disabled = true;
+
+const addLeadingZero = value => String(value).padStart(2, 0);
 
 const options = {
     enableTime: true,
@@ -11,14 +22,53 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
-
-  
+     if (selectedDates[0] < new Date()) {
+        buttonStart.disabled = true;
+         window.alert("Please choose a date in the future");
+         return;
+        } 
+        buttonStart.disabled = false;
+        setDate = selectedDates[0];
+     } 
+};
  
+function convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    return { days, hours, minutes, seconds };
+  }
+
+flatpickr(timePicker, options);
+
+buttonStart.addEventListener('click', timerStart);
+
+function timerStart () {
+    timerId = setInterval(() => {
+        if (selectedDates[0].getTime() - new Date().getTime() < 1000) {
+            clearInterval(timerId);
+            refs.buttonStart.disabled = true;
+            refs.timePicker.disabled = false;
+            return;
+        } else {
+            refs.buttonStart.disabled = true;
+            refs.timePicker.disabled = true;
+            new Date().getTime() += 1000;
+            remainingTime = Math.floor(selectedDates[0].getTime() - new Date().getTime());
+            convertMs(remainingTime);
+        }
+    }, 1000);
+}
 
 
 
 
-// console.log();
+
+
+
+

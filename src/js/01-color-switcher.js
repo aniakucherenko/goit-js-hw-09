11dc26223724eecaa731d00a6586781eb412b876
aniakucherenko <ania.kucherenko@gmail.com>
2023-05-30 17:58:48 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+startBtn.addEventListener('click', startBtnClick);
+
+function startBtnClick() {
+  timerId = setInterval(colorChanger, 1000);
+  function colorChanger() {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }
+}
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+});

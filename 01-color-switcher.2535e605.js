4d9a940c706d2309c9f5ae3cf94a568d898e145a
aniakucherenko <110539372+aniakucherenko@users.noreply.github.com>
2023-05-30 16:18:03 +0000

    Deploying to gh-pages from @ aniakucherenko/goit-js-hw-09@b312d1f6c8e465826d30cb898e9c3c82b8db0a00 🚀

--- 01-color-switcher.2535e605.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=null;t.addEventListener("click",(function(){e=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),n.addEventListener("click",(function(){clearInterval(e)}))}();
-//# sourceMappingURL=01-color-switcher.2535e605.js.map
--- 01-color-switcher.8aaeb835.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let n=null;t.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(()=>{clearInterval(n)}));
-//# sourceMappingURL=01-color-switcher.8aaeb835.js.map
